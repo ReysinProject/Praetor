754b1028bf3b8a923e9b1b3cc36e2464874017aa
ER28 <l.guerra-rago@pm.me>
2025-01-30 13:37:24 +0100

    wip

--- src/rule.ts
+++ src/rule.ts
@@ -1,3 +1,10 @@
 class Rule {
+    constructor(rule_name: string, func: (action: string, resource: string, context: any) => boolean) {
+        
+    }
 
+
+    evaluate(action: string, actor: any, resource: string, context: any) {
+        return false;
+    }
 }
